5076e751d852e8875f0b8e806c45e7eba4fa48aa
dan <lidan0001@gmail.com>
2022-01-16 22:43:51 -0500

    commit

---
+++ src/31.py
@@ -0,0 +1,6 @@
+import mnist_loader
+training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
+import network2
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(training_data , 30, 10, 0.5, evaluation_data=test_data , monitor_evaluation_accuracy=True)
